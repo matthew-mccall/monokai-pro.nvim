d2b2a1d212d022002e4f550453f5bbaf2ed17f04
Matthew McCall <m-mccall+git@outlook.com>
2025-04-04 19:02:28 -0400

    restore 2 indenting

--- lua/monokai-pro/util/extra.lua
+++ lua/monokai-pro/util/extra.lua
@@ -33,47 +33,47 @@ end
 
 -- Function to check if the system is macOS
 function isMacOS()
-    -- Execute the `uname` command to determine the operating system
-    local handle = io.popen("uname")
-    local result = handle:read("*a")
-    handle:close()
-    
-    -- Check if the result matches "Darwin" (the identifier for macOS)
-    return result:match("^%s*(.-)%s*$") == "Darwin"
+  -- Execute the `uname` command to determine the operating system
+  local handle = io.popen("uname")
+  local result = handle:read("*a")
+  handle:close()
+
+  -- Check if the result matches "Darwin" (the identifier for macOS)
+  return result:match("^%s*(.-)%s*$") == "Darwin"
 end
 
 -- Function to get the current macOS appearance (Light or Dark Mode)
 function getMacOSAppearance()
-    -- AppleScript to check the macOS appearance
-    local applescript = [[
-        tell application "System Events"
-            tell appearance preferences
-                if dark mode is true then
-                    return "Dark"
-                else
-                    return "Light"
-                end if
-            end tell
+  -- AppleScript to check the macOS appearance
+  local applescript = [[
+  tell application "System Events"
+  tell appearance preferences
+  if dark mode is true then
+    return "Dark"
+  else
+    return "Light"
+  end if
+end tell
         end tell
-    ]]
-    
-    -- Execute the AppleScript using os.execute or io.popen
-    local handle = io.popen('osascript -e \'' .. applescript .. '\'')
-    local result = handle:read("*a")
-    handle:close()
-    
-    -- Remove any trailing whitespace and return the result
-    return result:match("^%s*(.-)%s*$")
-end
+        ]]
 
-function M.is_daytime()
-    if isMacOS() then
-        return getMacOSAppearance() == "Light"
-    else
-        local current_time = os.time()
-        local current_hour = tonumber(os.date("%H", current_time))
-        return current_hour >= 6 and current_hour < 18
-    end
-end
+        -- Execute the AppleScript using os.execute or io.popen
+        local handle = io.popen('osascript -e \'' .. applescript .. '\'')
+        local result = handle:read("*a")
+        handle:close()
 
-return M
+        -- Remove any trailing whitespace and return the result
+        return result:match("^%s*(.-)%s*$")
+      end
+
+      function M.is_daytime()
+        if isMacOS() then
+          return getMacOSAppearance() == "Light"
+        else
+          local current_time = os.time()
+          local current_hour = tonumber(os.date("%H", current_time))
+          return current_hour >= 6 and current_hour < 18
+        end
+      end
+
+      return M
